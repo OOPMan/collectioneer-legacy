c0680812127160a7dc93e7190d5f6dc4ba7b1403
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2017-01-05 16:00:39 +0200

    Added code to generate fixture Collections.

--- src/main/scala/com/github/oopman/collectioneer/model/DAL.scala
+++ src/main/scala/com/github/oopman/collectioneer/model/DAL.scala
@@ -1,11 +1,22 @@
 package com.github.oopman.collectioneer.model
 
+import java.sql.Timestamp
+import java.time.Instant
+
 import slick.jdbc.JdbcProfile
 
 class DAL(val driver: JdbcProfile) extends Tables with DriverComponent {
   import driver.api._
 
+  val now = Timestamp.from(Instant.now)
+
   val create = (
     categories.schema ++ collections.schema ++ items.schema ++ collectionItems.schema ++
     collectionParentCollections.schema ++ tags.schema ++ tagCollections.schema ++ tagItems.schema).create
+
+  val populateCollections = collections.map(c => (c.name, c.dateTimeCreated, c.dateTimeModified)) ++= Seq(
+    ("Bits", now, now),
+    ("Bobs", now, now),
+    ("Misc", now, now)
+  )
 }
